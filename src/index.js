9704a53cc00d2bb55559011273476ad9cdbb4a46
ffran.acosta <francoaugustoacosta@gmail.com>
2022-08-08 01:48:41 -0300

    cambio html por ejs aplicando partials

--- src/index.js
+++ src/index.js
@@ -5,22 +5,20 @@ const port = process.env.PORT || 3000;
 const start = () => (console.log("Starting server..."));
 server.listen(port, start());
 
+server.set('views', './src/views');
+server.set('view engine', 'ejs');
+
 const statics = express.static(join(__dirname, "../public"));
 server.use(statics);
 
-server.set('views', './src/views');
-
-server.set('view engine', 'ejs');
-
-
 server.get("/", (req, res) => res.render('index'));
 
-server.get("/", (req, res) => res.render('login'));
+server.get("/login", (req, res) => res.render('login'));
 
-server.get("/", (req, res) => res.render('register'));
+server.get("/register", (req, res) => res.render('register'));
 
-server.get("/", (req, res) => res.render('cart'));
+server.get("/cart", (req, res) => res.render('cart'));
 
-server.get("/", (req, res) => res.render('products'));
+server.get("/products", (req, res) => res.render('products'));
 
 server.get("/", (req, res) => res.render('agregarProd'));
