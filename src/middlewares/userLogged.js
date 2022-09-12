90235bf25f2fc37cd318fa8a9723a4dc0c66bc0c
ffran.acosta <francoaugustoacosta@gmail.com>
2022-09-12 18:28:32 -0300

    merge gonza mio

--- src/middlewares/userLogged.js
+++ src/middlewares/userLogged.js
@@ -2,13 +2,8 @@ const userLogged = (req, res, next) => {
     
     if (req.session && req.session.user){
         return next()
-<<<<<<< HEAD
-        }
-        return res.redirect('/')
-=======
     }
 return res.redirect('/')
->>>>>>> 8aaf07586199e1df721e9fde261ac7a9da89a388
     
 }
 
