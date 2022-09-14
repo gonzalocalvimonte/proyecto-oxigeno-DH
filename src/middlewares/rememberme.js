30245ca886567c96089129a3f527649f787fd9ee
Gonzalo Calvimonte <gonzalocalvimonte@gmail.com>
2022-09-14 12:12:07 -0300

    prueba cookie

--- src/middlewares/rememberme.js
+++ src/middlewares/rememberme.js
@@ -1,13 +1,14 @@
-/* const {all} = require('../models/users.models')
+const {all} = require('../models/users.models')
 
 function rememberMe(req, res, next){
 
   if (req.cookies && req.cookies.rememberMe){
       let list = all();
       req.session.user = list.find(user => user.email == req.cookies.rememberMe);
-  } 
+      console.log (req.cookies.rememberMe)
+    } 
 
     return next();
 }
 
-module.exports = rememberMe; */
+module.exports = rememberMe;
