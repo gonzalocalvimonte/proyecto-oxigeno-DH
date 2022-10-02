40a285bb823f8b8e2640668e92c26f8454edaf8c
JuliLapido <lapidojulian@gmail.com>
2022-10-02 17:46:29 -0300

    moodelo de products mysql

---
+++ src/database/models/Products.js
@@ -0,0 +1,52 @@
+module.exports = function(sequelize, dataTypes  ) { 
+    let alias = "Products"
+    
+    let cols = {
+        sku:{
+            type: dataTypes.INTEGRER,
+            primaryKEY: true,
+            autoIncrement: true,
+
+        },
+        name:{
+            type: dataTypes.STRING
+
+        },
+        price:{
+            type: dataTypes.DOUBLE
+
+        },
+        descripcion:{
+            type: dataTypes.TEXT
+
+        },
+        image:{
+            type: dataTypes.STRING
+
+        },
+        category:{
+            type: dataTypes.INTEGRER
+
+        },
+    }
+
+    let config = {
+        tableName: "products",
+        timestamps: false,
+    }
+
+    let Products  = sequelize.define(alias, cols, config)
+
+    Products.associate = function (models) {
+        Products.belongsTo(models.categories, {
+            as: "categories",
+            foreingnKey: "cat_id",
+
+        } )
+
+    };
+
+    
+
+    return Products;
+    }
