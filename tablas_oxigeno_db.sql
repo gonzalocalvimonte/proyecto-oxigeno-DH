c336d4a3ef45fcf1a4987ac34201c1ae526fdc8b
lmftasende@gmail.com <lmftasende@gmail.com>
2022-10-03 18:30:37 -0300

    cambio a nombres de columna no redundantes

--- tablas_oxigeno_db.sql
+++ tablas_oxigeno_db.sql
@@ -5,10 +5,10 @@ create database oxigeno_db;
 use oxigeno_db;
 
 CREATE TABLE categories (
-  cat_id int(10) UNSIGNED NOT NULL,
-  cat_name varchar(50) DEFAULT NULL,
-  cat_description varchar(256) DEFAULT NULL,
-  primary key(cat_id)
+  id int(10) UNSIGNED NOT NULL,
+  name varchar(50) DEFAULT NULL,
+  description varchar(256) DEFAULT NULL,
+  primary key(id)
 ) ;
 
 CREATE TABLE products (
@@ -19,14 +19,14 @@ CREATE TABLE products (
   image varchar(128) NOT NULL,
   category int(10) UNSIGNED Not NULL,
   primary key (sku),
-  foreign key (category) references categories(cat_id)
+  foreign key (category) references categories(id)
 ) ;
 
 
 CREATE TABLE roles (
-  rol_id int(10) UNSIGNED NOT NULL,
-  rol_nombre varchar(50) DEFAULT NULL,
-  primary key (rol_id)
+  id int(10) UNSIGNED NOT NULL,
+  nombre varchar(50) DEFAULT NULL,
+  primary key (id)
 ) ;
 
 
@@ -41,5 +41,5 @@ CREATE TABLE users (
   avatar varchar(100) NOT NULL,
   rol int(10) UNSIGNED not NULL,
   primary key (id),
-  foreign key (rol) references roles (rol_id)
+  foreign key (rol) references roles (id)
 ) ;
