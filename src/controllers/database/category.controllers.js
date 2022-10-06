4933e93d4715276451e076de576a5950c2180e59
Mati_ toloo <matolox88@gmail.com>
2022-10-06 12:35:30 -0300

    crud secundario completo sin css

--- src/controllers/database/category.controllers.js
+++ src/controllers/database/category.controllers.js
@@ -1,33 +1,57 @@
 const { name } = require('ejs');
 let db = require ('../../database/models/index')
 const {
-    validationResult, body
+    validationResult
 } = require('express-validator');
 let categoryController = {
     crear: function (req, res) {
-            return res.render("database/creacionCategory");
+            return res.render("database/creacionCategoria");
     },
     guardado: function(req,res) {
-        let errors = validationResult(req);
-        //if (errors.isEmpty()) {
-        /*     let {
-            description,
-            name
-        } = req.body */
-            res.send(req.body)
-    /* db.categories.create({
-          description,
-          name
-        }) */
-
-        console.log(req.body)
-    //}
+    db.categories.create({
+          description: req.body.description,
+          name: req.body.category
+        })
+        .then(() => res.redirect('/dbCategory/listado'))
     },
     listado: function(req, res) {
         db.categories.findAll()
         .then(function(categories){
-        res.render('creacionCategory', {categories: categories});
+        res.render('database/listaDeCategorias', {categories: categories});
         })
+    },
+    detalle: function(req, res) {
+        db.categories.findByPk(req.params.id)
+        .then(function(categories){
+            res.render('database/detalleCategoria', {categories: categories} )
+        })
+    },
+    editar: function(req, res) {
+        let pedidoCategoria = db.categories.findByPk(req.params.id);
+
+        Promise.all([pedidoCategoria])
+            .then(function([categories]){
+                res.render('database/editarCategoria', {categories: categories})
+            })
+    },
+    actualizar: function(req, res) {
+            db.categories.update({
+                  description: req.body.description,
+                  name: req.body.category
+                }, {
+                    where: {
+                        id: req.params.id
+                    }
+                })
+        .then(() => res.redirect('/dbCategory/' + req.params.id))
+    },
+    borrar: function(req, res) {
+        db.categories.destroy({
+            where:{
+                id: req.params.id
+            }
+        })
+        .then(() => res.redirect('/dbCategory/listado'))
     }
 }
 
--- src/routes/database/category.routes.js
+++ src/routes/database/category.routes.js
@@ -2,11 +2,27 @@ let express = require('express');
 let router = express.Router();
 let categoryController = require('../../controllers/database/category.controllers')
 
-// Crear
+// Creación
+
 router.get('/crear', categoryController.crear);
 
 router.post('/guardado', categoryController.guardado);
-//read
-router.get('/listado', categoryController.listado)
+
+// Lectura
+
+router.get('/listado', categoryController.listado);
+
+// Detalle
+
+router.get('/:id', categoryController.detalle);
+
+// Actualización
+
+router.get('/editar/:id', categoryController.editar);
+router.post('/editar/:id', categoryController.actualizar);
+
+// Borrado
+
+router.post('/borrar/:id', categoryController.borrar);
 
 module.exports = router
