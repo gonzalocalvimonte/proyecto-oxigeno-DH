e39ec93e2e07c1b25d19a2fd67c9f5187c8a9279
lmftasende <106562986+lmftasende@users.noreply.github.com>
2022-11-13 12:50:55 -0300

    visibilidad o no de password

--- proyecto-oxigeno/public/scripts/login.js
+++ proyecto-oxigeno/public/scripts/login.js
@@ -28,4 +28,14 @@ window.addEventListener("load", function() {
             e.preventDefault(); 
         }
     })
+
+    let eyeButton = document.querySelector("#togglePasswordView");
+
+    eyeButton.addEventListener("click", function (e) {
+        
+        const type = password.getAttribute("type") === "password" ? "text" : "password";
+        password.setAttribute("type", type);
+        this.classList.toggle('fa-eye-slash');
+        
+    })
 })
